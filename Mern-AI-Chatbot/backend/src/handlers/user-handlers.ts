2a9974fba79196ff26472d5d2e2ef96be25e26bf
Ishan Kumarasinghe <e20211@eng.pdn.ac.lk>
2025-11-06 13:21:27 +0530

    Sign up function

--- Mern-AI-Chatbot/backend/src/handlers/user-handlers.ts
+++ Mern-AI-Chatbot/backend/src/handlers/user-handlers.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import User from "../models/User.js";
+import bcrypt from 'bcrypt';
 
 export const getAllUsers = async (
     req: Request,
@@ -9,6 +10,33 @@ export const getAllUsers = async (
     try {
         const users = await User.find();
         return res.status(200).json({ users });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ message: error.message });
+    }
+}
+
+export const signup = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    try {
+        const { name, email, password } = req.body;
+
+        //Validating new User
+        const existingUser = await User.findOne({ email });
+        if(existingUser){
+            return res.status(401).json({ message: "User already registered" });
+        }
+
+        //Password Encryption
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        //Signed up new user
+        const user = new User({ name, email, password:hashedPassword});
+        await user.save();
+        return res.status(201).json({ message: "Signed Up", user });
     } catch (error) {
         console.log(error);
         return res.status(500).json({ message: error.message });
