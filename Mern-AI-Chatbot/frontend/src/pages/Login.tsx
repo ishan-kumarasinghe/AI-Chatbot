1ba3a0bf82e7817fb0650dc898ed7cb99e2b7fd5
Ishan Kumarasinghe <e20211@eng.pdn.ac.lk>
2025-12-03 23:51:23 +0530

    Login Screen
    Start building Login screen

--- Mern-AI-Chatbot/frontend/src/pages/Login.tsx
+++ Mern-AI-Chatbot/frontend/src/pages/Login.tsx
@@ -1,5 +1,14 @@
+import { Box } from "@mui/material"
+
+import airRobot from "../assets/airobot.png";
+
 const Login = () => {
-    return <div>Login</div>
+    return <Box width={"100%"} height={"100%"} display="flex" flex={1}>
+        <Box padding={8} mt={8} display={{md: "flex", sm: "none", xs: "None"}}>
+            <img src={airRobot} alt="Robot" style={{width: "400px"}} />
+        </Box>
+        <Box></Box>
+    </Box>
 };
 
 export default Login;
