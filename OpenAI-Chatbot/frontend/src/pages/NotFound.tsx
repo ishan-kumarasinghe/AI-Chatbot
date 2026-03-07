3a785ea0b367b23b49ae5e54cd5f12225db2526f
Ishan Kumarasinghe <e20211@eng.pdn.ac.lk>
2026-03-07 12:38:59 +0530

    App file and not found page
    App file consist with header and routes to other files
    NotFound page is for route when request for undefined page

---
+++ OpenAI-Chatbot/frontend/src/pages/NotFound.tsx
@@ -0,0 +1,8 @@
+// Where shows this route is not registered (if a user try access using a typed path)
+import React from "react";
+
+const NotFound = () => {
+  return <div>NotFound</div>;
+};
+
+export default NotFound;
